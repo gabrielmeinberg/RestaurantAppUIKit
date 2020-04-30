b94b68f2dfee04a9ca1ea6fd298136cecf0bb3d3
Gabriel Meinberg Renó <48909683+gabrielmeinberg@users.noreply.github.com>
2020-04-30 17:42:25 -0300

    Revert "Feat/ddd"

--- lib/application_layer/use_case/product_use_case.dart
+++
@@ -1,8 +0,0 @@
-import 'package:restaurant_ui_kit/application_layer/adapters/product_adapter.dart';
-import 'package:restaurant_ui_kit/domain_layer/models/product_model.dart';
-
-class ProductUserCase {
-  Future<List<ProductModel>> getAllProducts() async{
-    return ProductService().getAll(ProductAdapterAPi());
-  }
-}
--- lib/domain_layer/ports/product_port.dart
+++
@@ -1,5 +0,0 @@
-import 'package:restaurant_ui_kit/domain_layer/models/product_model.dart';
-
-abstract class ProductPort{
-  Future<List<ProductModel>> getProducts(int id);
-}
